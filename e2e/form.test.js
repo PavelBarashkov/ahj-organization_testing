0ff67b02ccc7acc7cbdbc0c8f79fe49e08e6da4f
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2023-07-13 14:29:13 +0400

    update impirt in widget

--- e2e/form.test.js
+++ e2e/form.test.js
@@ -2,7 +2,7 @@
 import puppeteer from 'puppeteer';
 import { fork } from 'child_process';
 
-jest.setTimeout(100000);
+jest.setTimeout(50000);
 describe('Credit Card Validator form', () => {
   let browser;
   let page;
@@ -74,8 +74,4 @@ describe('Credit Card Validator form', () => {
     },
     15000,
   );
-
-  afterEach(async () => {
-    await browser.kill();
-  });
 });
